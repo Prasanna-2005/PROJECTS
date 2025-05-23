17f3404d9efb43ab96dc4bd64a0c3f437fe4c2f7
Prasanna <mpk19052005@gmail.com>
2025-05-23 17:30:35 +0530

    CONFIG

---
+++ WEB DEVELOPMENT/CINE_HIVE/CINE-HIVE(CLIENT)/Backend-cinehive/routes/toggle.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const toggleController = require('../controllers/toggle');
+
+router.get('/movie/toggle-like/:movie_id', toggleController.toggleLike);
+router.get('/movie/toggle-watchlist/:movie_id', toggleController.toggleWatchlist);
+
+module.exports = router;
